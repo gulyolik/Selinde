713392f530d490300951f54eb4b47eb55fb94d2d
gulyolik <olia.guliaeva@yandex.ru>
2023-01-25 20:56:42 +0300

    New fixies

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -50,42 +51,41 @@ public class CardDeliveryTest {
     }
 
 
-
-        @Test
-        void shouldCheckCityNameValidation() {
-            LocalDate date = LocalDate.now();
-            DateTimeFormatter formatters = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-            date = date.plusDays(5);
-            String planningDate = formatters.format(date);
-            Configuration.holdBrowserOpen = true;
-            open("http:/localhost:9999/");
-            $("[data-test-id = 'city'] input").setValue("Во");
-            $x("//*[contains(text(),'Вологда')]").click();
-            $("[data-test-id = 'date'] input").sendKeys((Keys.CONTROL + "A"), Keys.DELETE);
-            $("[data-test-id = 'date'] input").setValue(planningDate);
-            $(By.className("icon-button__content")).click();
-            $x("//*[contains(text(), '25')]").click();
-            $("[data-test-id = 'name'] input").setValue("Гуляева Ольга");
-            $("[data-test-id = 'phone'] input").setValue("+79967737496");
-            $(By.className("checkbox__box")).click();
-            $x("//*[contains(text(), 'Забронировать')]").click();
-            $("[data-test-id = 'notification']").should(appear, Duration.ofSeconds(12)).shouldBe(visible);
-        }
-
-        @Test
-        void shouldChooseDateWithNewWay() {
-            Configuration.holdBrowserOpen = true;
-            Configuration.timeout = 10000;
-            open("http:/localhost:9999/");
-            $("[data-test-id = 'city'] input").setValue("Вол");
-            $x("//*[contains(text(),'Вологда')]").click();
-            $(By.className("icon-button__content")).click();
-            this.planningDateGeneration(5);
-            $("[data-test-id = 'name'] input").setValue("Гуляева Ольга");
-            $("[data-test-id = 'phone'] input").setValue("+79967737496");
-            $(By.className("checkbox__box")).click();
-            $x("//*[contains(text(), 'Забронировать')]").click();
-            $("[data-test-id = 'notification']").should(appear, Duration.ofSeconds(12)).shouldBe(visible);
-        }
-
+    @Test
+    void shouldCheckCityNameValidation() {
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter formatters = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        date = date.plusDays(5);
+        String planningDate = formatters.format(date);
+        Configuration.holdBrowserOpen = true;
+        open("http:/localhost:9999/");
+        $("[data-test-id = 'city'] input").setValue("Во");
+        $x("//*[contains(text(),'Вологда')]").click();
+        $("[data-test-id = 'date'] input").sendKeys((Keys.CONTROL + "A"), Keys.DELETE);
+        $("[data-test-id = 'date'] input").setValue(planningDate);
+        $(By.className("icon-button__content")).click();
+        $x("//*[contains(text(), '25')]").click();
+        $("[data-test-id = 'name'] input").setValue("Гуляева Ольга");
+        $("[data-test-id = 'phone'] input").setValue("+79967737496");
+        $(By.className("checkbox__box")).click();
+        $x("//*[contains(text(), 'Забронировать')]").click();
+        $("[data-test-id = 'notification']").should(appear, Duration.ofSeconds(12)).shouldBe(visible);
     }
+
+    @Test
+    void shouldChooseDateWithNewWay() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 10000;
+        open("http:/localhost:9999/");
+        $("[data-test-id = 'city'] input").setValue("Вол");
+        $x("//*[contains(text(),'Вологда')]").click();
+        $(By.className("icon-button__content")).click();
+        this.planningDateGeneration(5);
+        $("[data-test-id = 'name'] input").setValue("Гуляева Ольга");
+        $("[data-test-id = 'phone'] input").setValue("+79967737496");
+        $(By.className("checkbox__box")).click();
+        $x("//*[contains(text(), 'Забронировать')]").click();
+        $("[data-test-id = 'notification']").should(appear, Duration.ofSeconds(12)).shouldBe(visible);
+    }
+
+}
